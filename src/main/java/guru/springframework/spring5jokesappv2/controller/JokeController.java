afae83a1c29419466d2fce1143479b0561c64af9
Daren <daren.peh.long@gmail.com>
2022-01-21 11:54:40 +0800

    first commit - added base files

---
+++ src/main/java/guru/springframework/spring5jokesappv2/controller/JokeController.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring5jokesappv2.controller;
+
+import guru.springframework.spring5jokesappv2.service.JokeService;
+
+public class JokeController {
+    private JokeService jokeService;
+    
+}
